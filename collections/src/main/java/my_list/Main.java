f9a0d63a9ed7ed3b6b96c3ebb9da479b212c89d5
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-04-01 18:32:16 +0300

    add main for list

---
+++ collections/src/main/java/my_list/Main.java
@@ -0,0 +1,59 @@
+package my_list;
+
+import net.datafaker.Faker;
+
+public class Main {
+    private static final Faker faker = new Faker();
+
+    public static void main(String[] args) {
+        checkMyArrayList();
+        //checkMyLinkedList();
+    }
+
+    private static void checkMyArrayList(){
+        MyArrayList myArrayList = new MyArrayList();
+        for (int i = 0; i < 15; ++i) {
+            myArrayList.add(i + 1);
+        }
+
+        System.out.println(myArrayList);
+        myArrayList.add(20, faker.random().nextInt());
+        System.out.println(myArrayList);
+        myArrayList.add(30, faker.random().nextInt());
+        System.out.println(myArrayList);
+
+        for (int i = 0; i < 10; ++i) {
+            myArrayList.add(faker.random().nextInt(myArrayList.size()), (i + 1) * 10);
+        }
+        System.out.println(myArrayList);
+
+        myArrayList.remove(5);
+        System.out.println(myArrayList);
+        myArrayList.remove(10);
+        System.out.println(myArrayList);
+        myArrayList.remove(15);
+        System.out.println(myArrayList);
+    }
+
+    private static void checkMyLinkedList() {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        for (int i = 0; i < 10; ++i) {
+            myLinkedList.add(i);
+        }
+
+        System.out.println(myLinkedList);
+
+        myLinkedList.add(3, faker.random().nextInt(50, 150));
+        System.out.println(myLinkedList);
+        myLinkedList.add(0, faker.random().nextInt(50, 150));
+        System.out.println(myLinkedList);
+        myLinkedList.add(-25, faker.random().nextInt(50, 150));
+        System.out.println(myLinkedList);
+        myLinkedList.add(25, faker.random().nextInt(50, 150));
+        System.out.println(myLinkedList);
+        myLinkedList.remove(5);
+        System.out.println(myLinkedList);
+        myLinkedList.clear();
+        System.out.println(myLinkedList);
+    }
+}
