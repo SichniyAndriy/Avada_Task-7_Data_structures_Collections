cf4abb0736208e645610fbfcdfeeccb3f29f5193
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-04-02 18:32:34 +0300

    add generic MyLinkedList code in main

--- collections_with_generics/src/main/java/my_list/Main.java
+++ collections_with_generics/src/main/java/my_list/Main.java
@@ -7,10 +7,47 @@ public class Main {
     private static final Faker faker = new Faker();
 
     public static void main(String[] args) {
-       testMyArrayList();
+       //testMyArrayList();
+       testMyLinkedList();
     }
 
-    static void testMyArrayList() {
+    private static void testMyLinkedList() {
+        MyList<Person> myLinkedList = new MyLinkedList<>();
+        for (int i = 0; i < 5; ++i) {
+            Person person = new Person(
+                    faker.idNumber().peselNumber(),
+                    faker.name().fullName(),
+                    faker.random().nextInt(15, 75)
+            );
+            myLinkedList.add(person);
+        }
+
+        for (int i = 0; i < 3; ++i) {
+            Person person = new Person(
+                    faker.idNumber().peselNumber(),
+                    faker.name().fullName(),
+                    faker.random().nextInt(15, 75)
+            );
+            myLinkedList.add(faker.random().nextInt(myLinkedList.size() - 1), person);
+        }
+
+        Person person;
+        int index;
+        person = myLinkedList.get(0);
+        myLinkedList.remove(person);
+        index = faker.random().nextInt(0, myLinkedList.size() - 1);
+        person = myLinkedList.get(index);
+        myLinkedList.remove(person);
+        index = faker.random().nextInt(0, myLinkedList.size() - 1);
+        person = myLinkedList.get(index);
+        myLinkedList.remove(person);
+        person = myLinkedList.get(myLinkedList.size() - 1);
+        myLinkedList.remove(person);
+
+        myLinkedList.clear();
+    }
+
+    private static void testMyArrayList() {
         MyList<Person> myArrayList = new MyArrayList<>(Person.class);
         for (int i = 0; i < 15; ++i) {
             Person person = new Person(
