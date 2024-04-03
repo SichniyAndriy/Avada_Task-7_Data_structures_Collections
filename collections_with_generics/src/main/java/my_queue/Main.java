ea82cc85f4526f0c6ca7072bf93f817b853ce521
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-04-03 11:32:51 +0300

    add client's code in main my_queue package

---
+++ collections_with_generics/src/main/java/my_queue/Main.java
@@ -0,0 +1,28 @@
+package main.java.my_queue;
+
+import main.java.Person;
+import net.datafaker.Faker;
+
+public class Main {
+    private final static Faker faker = new Faker();
+
+    public static void main(String[] args) {
+        MyQueue<Person> myArrayQueue = new MyLinkedQueue<>(Person.class);
+        for (int i = 0; i < 11; ++i) {
+            Person person = new Person(
+                    faker.idNumber().ssnValid(),
+                    faker.name().fullName(),
+                    faker.random().nextInt(15, 75)
+            );
+            myArrayQueue.offer(person);
+            System.out.println("\n" + myArrayQueue);
+        }
+
+        System.out.println("\n" + myArrayQueue.peek());
+
+        for (int i = 0; i < 5; ++i) {
+            myArrayQueue.poll();
+        }
+        System.out.println(myArrayQueue);
+    }
+}
