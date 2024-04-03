8bdc521e9b2d875427dd475940ab5f81b40c2e43
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-04-03 15:10:58 +0300

    add generic MyMap interface

---
+++ collections_with_generics/src/main/java/my_map/MyMap.java
@@ -0,0 +1,19 @@
+package main.java.my_map;
+
+public interface MyMap<K, V> {
+    int size();
+
+    boolean isEmpty();
+
+    void put (K key, V value);
+
+    boolean remove(K key);
+
+    void clear();
+
+    V get(K key);
+
+    K[] keyArray();
+
+    V[] valueArray();
+}
