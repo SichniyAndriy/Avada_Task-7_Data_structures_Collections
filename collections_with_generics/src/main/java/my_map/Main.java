ef67a9ed81cf61c57c0bd8290e2c67e6d4c66b79
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-04-03 15:11:46 +0300

    add client's code in my_map package

---
+++ collections_with_generics/src/main/java/my_map/Main.java
@@ -0,0 +1,34 @@
+package main.java.my_map;
+
+import net.datafaker.Faker;
+
+public class Main {
+    private final static Faker faker = new Faker();
+
+    public static void main(String[] args) {
+        MyMap<Long, String> myHashMap = new MyHashMap<>();
+
+        for (int i = 0; i < 5; i++) {
+            myHashMap.put(faker.barcode().gtin14(), faker.starWars().character());
+            myHashMap.put(faker.barcode().gtin14(), faker.stargate().characters());
+            myHashMap.put(faker.barcode().gtin14(), faker.bigBangTheory().character());
+            myHashMap.put(faker.barcode().gtin14(), faker.backToTheFuture().character());
+        }
+
+        Object[] keys = myHashMap.keyArray();
+        Object[] values = myHashMap.valueArray();
+
+        for (int i = 0; i < 3; i++) {
+            myHashMap.put(
+                    (Long) keys[faker.random().nextInt(keys.length)],
+                    (String) values[faker.random().nextInt(values.length)]
+            );
+        }
+
+        for (int i = 0; i < 3; ++i) {
+            myHashMap.remove((Long) keys[faker.random().nextInt(keys.length)]);
+        }
+
+        myHashMap.clear();
+    }
+}
